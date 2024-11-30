95673ce56261a3823bd7eefd931b30c66cb0a23b
Nazar Khmel <nazkhmel@gmail.com>
2024-11-30 14:16:50 +0100

    F

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hello World App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HelloWorldPage(),
+    );
+  }
+}
+
+class HelloWorldPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Hello World'),
+      ),
+      body: Center(
+        child: Text(
+          'Hello, World!',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
+    );
+  }
+}
